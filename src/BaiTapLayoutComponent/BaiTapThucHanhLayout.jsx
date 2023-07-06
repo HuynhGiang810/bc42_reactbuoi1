c2b34cca9804192962c2f14da6845ddc9467c684
Huynh Giang <huynhgiang@tanhuythinh.com>
2023-07-06 17:16:34 +0700

    done

---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Banner from './Banner'
+import ListItem from './ListItem'
+import Footer from './Footer'
+
+function BaiTapThucHanhLayout() {
+  return (
+    <>
+        <Header/>
+        <Banner/>
+        <ListItem/>
+        <Footer/>
+    </>
+  )
+}
+
+export default BaiTapThucHanhLayout
---
+++ src/BaiTapLayoutComponent/ListItem.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Item from './Item';
+
+function ListItem() {
+  return (
+    <section className="py-5">
+  <div className="container px-4 px-lg-5 mt-5">
+    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+       <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+      <div className="col mb-5">
+        <Item/>
+      </div>
+    </div>
+  </div>
+</section>
+
+  );
+}
+
+export default ListItem
